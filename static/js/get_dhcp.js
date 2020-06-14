f1a4e082f02bfb7ba8b90231a7d9940d8d5efe72
SamAlJuMa <samuelaljuma@gmail.com>
2020-06-14 03:10:31 -0500

    Interfaz para DHCP, crear y eliminar subredes

--- static/js/get_dhcp.js
+++ static/js/get_dhcp.js
@@ -1,9 +1,143 @@
 
-$(document).ready(function(){
-   
+$(document).ready(function () {
+
     $('.collapsible').collapsible({
-        onOpenEnd : function(arg){
-            alert (arg.value);
+        onOpenEnd: function (arg) {
+            //alert (arg.value);
         }
     });
-  });
+
+    $("body").delegate(".remBtn", "click", function () {
+        var parent_id = $(this).parent().parent();
+        parent_id.remove()
+    });
+
+    $('.addBtn').on('click', function () {
+        var parent_id = $(this).parent().parent().parent();
+        var ch = $(parent_id).children();
+        var num = ch.length;
+        //New li element
+        var newLi = document.createElement("li")
+
+        //Header
+        var liDivHeader = document.createElement("div")
+        $(liDivHeader).addClass("collapsible-header my-colapsible");
+
+        //Right icon
+        var iconCont = document.createElement("i");
+        $(iconCont).addClass("material-icons");
+        $(iconCont).text("settings_input_component");
+        $(liDivHeader).append(iconCont);
+
+        //Paragraph
+        var par = document.createElement("p");
+        $(par).text("Subred: xxxx.xxxx.xxxx.xxxx");
+        $(liDivHeader).append(par);
+
+        //"Remove" Button
+        var remBtn = document.createElement("a");
+        $(remBtn).addClass("waves-effect red btn-small remBtn");
+        var remBtnIcon = document.createElement("i");
+        $(remBtnIcon).addClass("material-icons right");
+        $(remBtnIcon).text("cancel");
+        $(remBtn).append(remBtnIcon);
+        $(liDivHeader).append(remBtn);
+
+        //Body
+        var liDivBody = document.createElement("div");
+        $(liDivBody).addClass("collapsible-body");
+        //Collection
+        var collection = document.createElement("ul");
+        $(collection).addClass("collection");
+        $(liDivBody).append(collection);
+
+        //Collectiion-item
+        var collItem = document.createElement("li");
+        $(collItem).addClass("collection-item");
+        //Collection-div
+        var collDiv = document.createElement("div");
+        $(collDiv).addClass("my-collection");
+        //InputField
+        var inputField = document.createElement("div");
+        $(inputField).addClass("input-field");
+        //Input
+        var input = document.createElement("input");
+        $(input).attr({
+            disabled: "",
+            type: "text",
+            value: "xxxx.xxxx.xxxx.xxxx",
+            id: "ip-start" + num,
+            class: "validate"
+        })
+        //Label
+        var label = document.createElement("label");
+        $(label).attr({
+            for: "ip-start" + num,
+            class: "active"
+        })
+        $(label).text("Inicio del Rango:");
+        //Button
+        var editBtn = document.createElement("button");
+        $(editBtn).addClass("secondary-content my-btn");
+        //Button-icon
+        var btnIcon = document.createElement("i");
+        $(btnIcon).addClass("material-icons");
+        $(btnIcon).text("create");
+        $(editBtn).append(btnIcon);
+
+        $(inputField).append(input);
+        $(inputField).append(label);
+        $(collDiv).append(inputField);
+        $(collDiv).append(editBtn);
+        $(collItem).append(collDiv);
+        $(collection).append(collItem);
+
+        //Collectiion-item
+        var collItem = document.createElement("li");
+        $(collItem).addClass("collection-item");
+        //Collection-div
+        var collDiv = document.createElement("div");
+        $(collDiv).addClass("my-collection");
+        //InputField
+        var inputField = document.createElement("div");
+        $(inputField).addClass("input-field");
+        //Input
+        var input = document.createElement("input");
+        $(input).attr({
+            disabled: "",
+            type: "text",
+            value: "xxxx.xxxx.xxxx.xxxx",
+            id: "ip-end" + num,
+            class: "validate"
+        })
+        //Label
+        var label = document.createElement("label");
+        $(label).attr({
+            for: "ip-end" + num,
+            class: "active"
+        })
+        $(label).text("Fin del Rango:");
+        //Button
+        var editBtn = document.createElement("button");
+        $(editBtn).addClass("secondary-content my-btn");
+        //Button-icon
+        var btnIcon = document.createElement("i");
+        $(btnIcon).addClass("material-icons");
+        $(btnIcon).text("create");
+        $(editBtn).append(btnIcon);
+
+        $(inputField).append(input);
+        $(inputField).append(label);
+        $(collDiv).append(inputField);
+        $(collDiv).append(editBtn);
+        $(collItem).append(collDiv);
+        $(collection).append(collItem);
+
+
+        $(newLi).append(liDivHeader);
+        $(newLi).append(liDivBody);
+        $(parent_id).append(newLi);
+        //alert(num);
+    })
+});
+
